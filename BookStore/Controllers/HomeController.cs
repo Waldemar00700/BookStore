8d218df626a0545e8fc09ef76a1e090b45b4e556
Waldemar00700 <64745097+Waldemar00700@users.noreply.github.com>
2021-02-24 14:55:59 +0300

    Добавьте файлы проекта.

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+
+            /*ViewBag представляет такой объект, который позволяет определить любую переменную 
+             * и передать ей некоторое значение, а затем в представлении извлечь это значение. 
+             * Так, мы определяем переменную ViewBag.Books, которая и будет хранить набор книг.*/
+
+            // возвращаем представление
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+    }
+}
